bba8ffe1f665a412d75f2ddf515df3560ad3c6ec
Avantika-Murali <avantikam.23aim@kongu.edu>
2026-03-14 16:05:43 +0530

    added predict route

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 from flask import Flask, render_template, request
 import os
+import pickle
+import numpy as np
 
 app = Flask(__name__)
+
+model = pickle.load(open("model.pkl", "rb"))
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    features = [float(x) for x in request.form.values()]
+    final_features = [np.array(features)]
+    prediction = model.predict(final_features)
+
+    output = prediction[0]
+
+    return render_template("index.html", prediction_text=f"Prediction: {output}")
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))  # Railway will override this
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
