d192643ae27dba9eb7fb3612c742e9372ee5c9f3
Avantika-Murali <avantikam.23aim@kongu.edu>
2026-03-14 16:23:33 +0530

    Updated model to LinearRegression for marks prediction

--- train.py
+++ train.py
@@ -1,30 +1,31 @@
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
 import pickle
 import mlflow
 import mlflow.sklearn
 
+# Example data for study hours and corresponding marks
 data = {
-    "hours":[1,2,3,4,5,6,7,8],
-    "pass":[0,0,0,0,1,1,1,1]
+    "hours": [1, 2, 3, 4, 5, 6, 7, 8],
+    "marks": [20, 30, 40, 50, 60, 70, 80, 90]  # numeric marks, continuous
 }
 
 df = pd.DataFrame(data)
 
 X = df[["hours"]]
-y = df["pass"]
+y = df["marks"]
 
-model = LogisticRegression()
+model = LinearRegression()
 
 mlflow.start_run()
 
-model.fit(X,y)
+model.fit(X, y)
 
-mlflow.log_param("model","LogisticRegression")
+mlflow.log_param("model", "LinearRegression")
 
-mlflow.sklearn.log_model(model,"model")
+mlflow.sklearn.log_model(model, "model")
 
-pickle.dump(model, open("model.pkl","wb"))
+pickle.dump(model, open("model.pkl", "wb"))
 
 mlflow.end_run()
 
